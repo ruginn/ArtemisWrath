8b344b55c4fe788f7b22de26e14fd8b0542948a7
Rcatarata <Rcatarata@gmail.com>
2023-12-09 18:52:57 -0500

    Initial Commit

---
+++ artemis-wrath/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <div>
+                <h1>Artemis&apos; Wrath</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
