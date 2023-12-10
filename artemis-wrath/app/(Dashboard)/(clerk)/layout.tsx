3343136edf8c96cf3afd197580e20efae4ed5314
Rcatarata <Rcatarata@gmail.com>
2023-12-09 20:30:46 -0500

    Create layout.tsx

---
+++ artemis-wrath/app/(Dashboard)/(clerk)/layout.tsx
@@ -0,0 +1,9 @@
+const ClerkLayout = ({children} : {children: React.ReactNode}) => {
+    return (
+        <div className="flex justify-center content-center">
+            {children}
+        </div>
+    )
+}
+
+export default ClerkLayout
