8e88d22e881ae62b7cc4f9df389d4056ac88adfd
Rcatarata <Rcatarata@gmail.com>
2024-01-11 18:40:00 -0500

    Card component

---
+++ artemis-wrath/app/components/Card.tsx
@@ -0,0 +1,42 @@
+import Image,{StaticImageData} from "next/image"
+import { cn } from "@/lib/utils"
+
+
+interface Card {
+    id: number, 
+    name: string, 
+    description: string, 
+    randomNumber: number,
+    inclination?: string
+    image?: string | StaticImageData, 
+}
+
+const CardElement = ({card}:{card:Card}) => {
+    return (
+        <div className={cn("h-96 w-72 bg-amber-200 flex flex-col rounded-md items-center border-gray-800 border-8", 
+            card.inclination === 'Wrath' && 'bg-red-800',
+            card.inclination === 'Wisdom' && 'bg-blue-400',
+            card.inclination === 'Love' && 'bg-pink-300', 
+            card.inclination === 'Mischief' && 'bg-gray-600', 
+            card.inclination === 'Pride' && 'bg-purple-600'
+        )} key={card.id}>
+                <h1 className="text-l self-start ml-2">{card.name}</h1>
+                {card.image &&
+                <Image 
+                src={card.image}
+                alt=''
+                width={256}
+                height={256}
+                placeholder='empty'
+                className='object-cover rounded-lg'
+                // className="w-60 h-auto"
+                ></Image>
+            }
+                <div className="w-[250px] h-[70px] border-2 border-black mt-3 rounded bg-gray-100 opacity-50">
+                    <p className="text-[11px] leading-none">{card.description}</p>
+                </div>
+        </div>
+    )
+}
+
+export default CardElement
--- artemis-wrath/app/dashboard/(routes)/exploresets/medievalcreatures/page.tsx
+++ artemis-wrath/app/dashboard/(routes)/exploresets/medievalcreatures/page.tsx
@@ -3,6 +3,7 @@ import Image, { StaticImageData } from 'next/image'
 import {useState, useEffect} from 'react'
 import { useSideBar } from "@/hooks/use-sidebar"
 import { cn } from '@/lib/utils'
+import CardElement from '@/app/components/Card'
 
 
 
@@ -52,33 +53,34 @@ const MedievalCreaturesSet = () => {
     return (
         <div>
             <h1>Medieval Creatures</h1>
-            <div className="grid grid-cols-5 gap-5 m-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 m-10">
             {cardsLoaded &&
                 cardSet.map((card) => {
                     return (
-                        <div className={cn("h-96 w-72 bg-amber-200 flex flex-col rounded-md items-center border-gray-800 border-8", 
-                            card.inclination === 'Wrath' && 'bg-red-800',
-                            card.inclination === 'Wisdom' && 'bg-blue-400',
-                            card.inclination === 'Love' && 'bg-pink-300', 
-                            card.inclination === 'Mischief' && 'bg-gray-600', 
-                            card.inclination === 'Pride' && 'bg-purple-600'
-                        )} key={card.id}>
-                                <h1 className="text-l self-start ml-2">{card.name}</h1>
-                                {card.image &&
-                                <Image 
-                                src={card.image}
-                                alt=''
-                                width={256}
-                                height={256}
-                                placeholder='empty'
-                                className='object-cover rounded-lg'
-                                // className="w-60 h-auto"
-                                ></Image>
-                            }
-                                <div className="w-[250px] h-[70px] border-2 border-black mt-3 rounded">
-                                    <p className="text-[11px] leading-none">{card.description}</p>
-                                </div>
-                            </div>
+                        // <div className={cn("h-96 w-72 bg-amber-200 flex flex-col rounded-md items-center border-gray-800 border-8", 
+                        //     card.inclination === 'Wrath' && 'bg-red-800',
+                        //     card.inclination === 'Wisdom' && 'bg-blue-400',
+                        //     card.inclination === 'Love' && 'bg-pink-300', 
+                        //     card.inclination === 'Mischief' && 'bg-gray-600', 
+                        //     card.inclination === 'Pride' && 'bg-purple-600'
+                        // )} key={card.id}>
+                        //         <h1 className="text-l self-start ml-2">{card.name}</h1>
+                        //         {card.image &&
+                        //         <Image 
+                        //         src={card.image}
+                        //         alt=''
+                        //         width={256}
+                        //         height={256}
+                        //         placeholder='empty'
+                        //         className='object-cover rounded-lg'
+                        //         // className="w-60 h-auto"
+                        //         ></Image>
+                        //     }
+                        //         <div className="w-[250px] h-[70px] border-2 border-black mt-3 rounded bg-gray-100 opacity-50">
+                        //             <p className="text-[11px] leading-none">{card.description}</p>
+                        //         </div>
+                        //     </div>
+                        <CardElement card={card}/>
                     )
                 })
                 
