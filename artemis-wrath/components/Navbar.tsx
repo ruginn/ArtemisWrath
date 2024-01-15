1189e569547671d4d167f28447fb6af2f44b6354
Rcatarata <Rcatarata@gmail.com>
2024-01-15 10:37:14 -0500

    Update Navbar.tsx
    mobile

--- artemis-wrath/components/Navbar.tsx
+++ artemis-wrath/components/Navbar.tsx
@@ -8,21 +8,21 @@ const NavBar = () => {
         // <nav className="h-24 flex items-center bg-gradient-to-r from-sky-950 via-cyan-950 to-gray-900">
         <nav className="h-20 flex items-center bg-gray-900 justify-between fixed z-[99999] w-full -mt-20">
             {/* <p className='from-sky-950 via-cyan-950 to-gray-900'></p> */}
-            <div className='flex flex-row items-center ml-5'>
+            <div className='flex flex-row items-center sm:ml-5 ml-3'>
                 <Image 
                     src={Logo}
                     alt='Logo'
-                    className='w-16 h-16'
+                    className='w-10 h-10 sm:w-16 sm:h-16'
                 />
                 <Image 
                     src={WordLogo}
                     alt='Artemis&apos; Wrath'
-                    className='h-10 w-64 ml-3'
+                    className='h-8 w-56 ml-1 sm:h-10 sm:w-64 sm:ml-3'
                 />
                 {/* <h1 className="text-white ml-2">Artemis&apos; Wrath</h1> */}
             </div>
             <div>
-                <button className="text-sm text-white mr-10">
+                <button className="text-sm text-white sm:mr-10 mr-5">
                     <SignedIn>
                        <UserButton afterSignOutUrl="/" />
                     </SignedIn>
