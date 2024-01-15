3756176cae596655ec3cb0eec9c589df7ba7669d
Rcatarata <Rcatarata@gmail.com>
2024-01-15 13:26:37 -0500

    side bar

--- artemis-wrath/components/Navbar.tsx
+++ artemis-wrath/components/Navbar.tsx
@@ -2,13 +2,20 @@ import Logo from '@/public/Images/AWLOGO2.png'
 import WordLogo from '@/public/Images/WordLogo.png'
 import Image from 'next/image'
 import { SignInButton, UserButton, SignedIn, SignedOut } from "@clerk/nextjs"
+import { Menu } from 'lucide-react'
+
 
 const NavBar = () => {
+
+
     return (
         // <nav className="h-24 flex items-center bg-gradient-to-r from-sky-950 via-cyan-950 to-gray-900">
         <nav className="h-20 flex items-center bg-gray-900 justify-between fixed z-[99999] w-full -mt-20">
             {/* <p className='from-sky-950 via-cyan-950 to-gray-900'></p> */}
             <div className='flex flex-row items-center sm:ml-5 ml-3'>
+                <SignedIn>
+                    <Menu className='sm:hidden text-amber-400'/>
+                </SignedIn>
                 <Image 
                     src={Logo}
                     alt='Logo'
@@ -22,7 +29,7 @@ const NavBar = () => {
                 {/* <h1 className="text-white ml-2">Artemis&apos; Wrath</h1> */}
             </div>
             <div>
-                <button className="text-sm text-white sm:mr-10 mr-5">
+                <button className="text-lg text-amber-400 sm:mr-10 mr-5">
                     <SignedIn>
                        <UserButton afterSignOutUrl="/" />
                     </SignedIn>
