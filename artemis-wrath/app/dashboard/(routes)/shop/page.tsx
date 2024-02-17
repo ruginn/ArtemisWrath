5facc26be2e5424eed6260284dbc4e0d185ae0f6
Rcatarata <Rcatarata@gmail.com>
2024-02-17 15:44:22 -0500

    Update page.tsx
    crystals

--- artemis-wrath/app/dashboard/(routes)/shop/page.tsx
+++ artemis-wrath/app/dashboard/(routes)/shop/page.tsx
@@ -17,35 +17,35 @@ const Settings = () => {
   return (
     <div className='w-screen sm:w-[calc(100vw-96px)] lg:w-[calc(100vw-256px)] flex flex-col items-center justify-center'>
       <h1 className='text-6xl'>Welcome to the Shop!</h1>
-      <div className='flex w-full justify-center gap-[10%] m-8'>
-        <div className='cursor-pointer w-1/4 h-[500px] flex flex-col items-center bg-gray-900 rounded-3xl border-4 border-gray-500'>
+      <div className='flex w-full justify-center gap-8 m-8'>
+        <div className='cursor-pointer w-1/4 h-[650px] flex flex-col items-center bg-gray-900 rounded-3xl border-4 border-gray-300 hover:scale-105'>
           <h1 className='text-2xl text-amber-400'>Small Chest</h1>
           <Image
             src={smallChest}
             width={256}
             height={256}
             alt='smallChest'
-            className='rounded-3xl border-amber-400 border-solid border-8'
+            className='rounded-3xl border-amber-400 border-solid border-2'
           ></Image>
         </div>
-        <div className='cursor-pointer w-1/4 h-[500px] flex flex-col items-center bg-gray-900 rounded-3xl border-4 border-gray-500'>
+        <div className='cursor-pointer w-1/4 h-[650px] flex flex-col items-center bg-gray-900 rounded-3xl border-4 border-gray-300 hover:scale-105'>
           <h1 className='text-2xl text-amber-400'>Medium Chest</h1>
           <Image
             src={mediumChest}
             width={256}
             height={256}
             alt='smallChest'
-            className='rounded-3xl border-amber-400 border-solid border-8'
+            className='rounded-3xl border-amber-400 border-solid border-2'
           ></Image>
         </div>
-        <div className='cursor-pointer w-1/4 h-[500px] flex flex-col items-center bg-gray-900 rounded-3xl border-4 border-gray-500'>
+        <div className='cursor-pointer w-1/4 h-[650px] flex flex-col items-center bg-gray-900 rounded-3xl border-4 border-gray-300 hover:scale-105'>
           <h1 className='text-2xl text-amber-400'>Large Chest</h1>
           <Image
             src={largeChest}
             width={256}
             height={256}
             alt='smallChest'
-            className='rounded-3xl border-amber-400 border-solid border-8'
+            className='rounded-3xl border-amber-400 border-solid border-2'
           ></Image>
         </div>
       </div>
