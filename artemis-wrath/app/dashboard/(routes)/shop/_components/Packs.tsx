6ce4cc19f29c34f158059791992fea19e71a424f
Rcatarata <Rcatarata@gmail.com>
2024-03-21 21:58:58 -0400

    Update Packs.tsx

--- artemis-wrath/app/dashboard/(routes)/shop/_components/Packs.tsx
+++ artemis-wrath/app/dashboard/(routes)/shop/_components/Packs.tsx
@@ -8,23 +8,40 @@ const ShopPacks = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
       <h1>Shop Packs</h1>
-      <div className='flex flex-row gap-4 w-full justify-center'>
-        <Image
-          src={AwSet1}
-          alt='Medieval Creatures'
-          className='w-[20%] h-auto border-t-[12px] border-b-[12px] border-gray-400 cursor-pointer'
-        />
-
-        <Image
-          src={AwSet2}
-          alt='Bushido Blades'
-          className='w-[20%] h-auto border-t-[12px] border-b-[12px] border-gray-400 cursor-pointer'
-        />
-        <Image
-          src={AwSet3}
-          alt='Desert Past'
-          className='w-[20%] h-auto border-t-[12px] border-b-[12px] border-gray-400 cursor-pointer'
-        />
+      <div className='flex flex-col md:flex-row gap-4 w-full justify-center items-center my-8'>
+        <div className='flex flex-col bg-gray-300 p-6 rounded-lg hover:scale-105 cursor-pointer'>
+          <Image
+            src={AwSet1}
+            alt='Medieval Creatures'
+            height={450}
+            className=' border-t-[12px] border-b-[12px] border-gray-400 cursor-pointer'
+          />
+          <h1 className='text-xl'>Artemis Wrath</h1>
+          <h3 className='text-l'>Medieval Creatures</h3>
+          <p>100 Crystals</p>
+        </div>
+        <div className='flex flex-col bg-gray-300 p-6 rounded-lg hover:scale-105 cursor-pointer'>
+          <Image
+            src={AwSet2}
+            alt='Bushido Blades'
+            height={450}
+            className='border-t-[12px] border-b-[12px] border-gray-400 cursor-pointer'
+          />
+          <h1 className='text-xl'>Artemis Wrath</h1>
+          <h3 className='text-l'>Bushido Blades</h3>
+          <p>100 Crystals</p>
+        </div>
+        <div className='flex flex-col bg-gray-300 p-6 rounded-lg hover:scale-105 cursor-pointer'>
+          <Image
+            src={AwSet3}
+            alt='Desert Past'
+            height={450}
+            className='border-t-[12px] border-b-[12px] border-gray-400 cursor-pointer'
+          />
+          <h1 className='text-xl'>Artemis Wrath</h1>
+          <h3 className='text-l'>Desert Past</h3>
+          <p>100 Crystals</p>
+        </div>
       </div>
     </div>
   );
