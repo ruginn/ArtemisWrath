72615800443f42f7408929c20c80dc8758307c43
Rcatarata <Rcatarata@gmail.com>
2024-04-07 20:50:53 -0400

    filtering trade center

--- artemis-wrath/app/dashboard/(routes)/tradecenter/trade/page.tsx
+++ artemis-wrath/app/dashboard/(routes)/tradecenter/trade/page.tsx
@@ -4,11 +4,16 @@ import { Card } from '@/types/CardType';
 import CardElement from '@/app/components/Card';
 import { useUser } from '@clerk/nextjs';
 import Loader from '@/app/dashboard/_components/Loader';
+import CardDetailModal from '@/app/components/CardDetailModal';
 
 const Trade = () => {
   const [allCards, setAllCards] = useState<Card[]>([]);
+  const [filterAll, setFilterAll] = useState<Card[]>([]);
+  const [filterQueryAll, setFilterQueryAll] = useState<string>('');
   const [loadingAllCards, setLoadingAllCards] = useState<boolean>(true);
   const [yourCards, setYourCards] = useState<Card[]>([]);
+  const [filterYour, setFilterYour] = useState<Card[]>([]);
+  const [filterQueryYour, setFilterQueryYour] = useState<string>('');
   const [loadingYourCards, setLoadingYourCards] = useState<boolean>(true);
   const { user } = useUser();
   useEffect(() => {
@@ -46,28 +51,94 @@ const Trade = () => {
     GetYourCards();
   }, []);
 
+  const FilterSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterQueryAll(e.target.value);
+  };
+
+  const FilterSearchYour = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterQueryYour(e.target.value);
+  };
+
+  useEffect(() => {
+    setFilterAll(
+      allCards.filter(
+        (cards) =>
+          cards.name?.toLowerCase().includes(filterQueryAll.toLowerCase()) ||
+          cards.biome?.toLowerCase().includes(filterQueryAll.toLowerCase()) ||
+          cards.type?.toLowerCase().includes(filterQueryAll.toLowerCase()) ||
+          cards.inclination
+            ?.toLowerCase()
+            .includes(filterQueryAll.toLowerCase()) ||
+          cards.inclination2
+            ?.toLowerCase()
+            .includes(filterQueryAll.toLowerCase()) ||
+          cards.rarity?.toLowerCase().includes(filterQueryAll.toLowerCase())
+      )
+    );
+  }, [filterQueryAll]);
+
+  useEffect(() => {
+    setFilterYour(
+      yourCards.filter(
+        (cards) =>
+          cards.name?.toLowerCase().includes(filterQueryYour.toLowerCase()) ||
+          cards.biome?.toLowerCase().includes(filterQueryYour.toLowerCase()) ||
+          cards.type?.toLowerCase().includes(filterQueryYour.toLowerCase()) ||
+          cards.inclination
+            ?.toLowerCase()
+            .includes(filterQueryYour.toLowerCase()) ||
+          cards.inclination2
+            ?.toLowerCase()
+            .includes(filterQueryYour.toLowerCase()) ||
+          cards.rarity?.toLowerCase().includes(filterQueryYour.toLowerCase())
+      )
+    );
+  }, [filterQueryYour]);
+
   return (
     <div>
-      <h1>Which card(s) are you looking for?</h1>
-      <label>Filter Cards</label>
-      <input type='text' className='outline-1 border-2' />
-      <div className='h-[50vh] overflow-y-scroll grid grid-cols-5 sm:grid-cols-6 md:grid-cols-9 lg:grid-cols-12 gap-5 m-10 border-gray-800 p-2 border-2 border-solid rounded-md justify-center items-center'>
+      <h1 className='text-4xl'>Create a trade?</h1>
+      <label htmlFor='allFilter'>Which card(s) are you looking for?</label>
+      <input
+        type='text'
+        className='outline-1 border-2'
+        value={filterQueryAll}
+        onChange={FilterSearch}
+        id='allFilter'
+      />
+      <div className='h-[50vh] overflow-y-scroll grid grid-cols-5 sm:grid-cols-6 md:grid-cols-9 lg:grid-cols-12 gap-5 m-10 border-gray-800 p-2 border-2 border-solid rounded-md justify-center '>
         {loadingAllCards && <Loader />}
         {!loadingAllCards &&
+          !filterQueryAll &&
           allCards.map((card) => {
             return <CardElement card={card} key={card.id} />;
           })}
-      </div>
-      <h1>Which card(s) are you offering?</h1>
-      <label>Filter Cards</label>
-      <input type='text' className='outline-1 border-2' />
-      <div className='h-[50vh] overflow-y-scroll grid grid-cols-5 sm:grid-cols-6 md:grid-cols-9 lg:grid-cols-12 gap-5 m-10 border-gray-800 p-2 border-2 border-solid rounded-md justify-center items-center'>
-        {loadingYourCards && <Loader />}
-        {!loadingYourCards &&
-          yourCards.map((card) => {
+        {filterQueryAll &&
+          filterAll.map((card) => {
             return <CardElement card={card} key={card.id} />;
           })}
       </div>
+      <label htmlFor='yourCards'>Which card(s) are you offering?</label>
+      <input
+        type='text'
+        className='outline-1 border-2'
+        value={filterQueryYour}
+        onChange={FilterSearchYour}
+        id='yourCards'
+      />
+      <div className='h-[50vh] overflow-y-scroll grid grid-cols-5 sm:grid-cols-6 md:grid-cols-9 lg:grid-cols-12 gap-5 m-10 border-gray-800 p-2 border-2 border-solid rounded-md justify-center'>
+        {loadingYourCards && <Loader />}
+        {!loadingYourCards &&
+          !filterQueryYour &&
+          yourCards.map((card) => {
+            return <CardElement card={card} key={card.id} />;
+          })}
+        {filterQueryYour &&
+          filterYour.map((card) => {
+            return <CardElement card={card} key={card.id} />;
+          })}
+      </div>
+      {/* <CardDetailModal /> */}
     </div>
   );
 };
