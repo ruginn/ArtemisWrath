1418f18cc35b2f4703788bc4960c3305ef680fb5
Rcatarata <Rcatarata@gmail.com>
2024-06-15 22:36:41 -0400

    Update page.tsx

--- artemis-wrath/app/dashboard/(routes)/house/create/page.tsx
+++ artemis-wrath/app/dashboard/(routes)/house/create/page.tsx
@@ -1,16 +1,45 @@
+'use client';
 import { Button } from '@/components/ui/button';
+import { useState } from 'react';
 
 const CreateHouse = () => {
+  const [houseName, setHouseName] = useState('');
+  const [houseSummary, setHouseSummary] = useState('');
+
+  const handleHouseName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHouseName(e.target.value);
+  };
+
+  const handleSummary = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setHouseSummary(e.target.value);
+  };
+
+  const onSubmit = () => {
+    console.log(houseName, houseSummary);
+  };
+
   return (
     <div>
       <h1>Create a House</h1>
-      <form action=''>
-        <p>What will be the name of the House?</p>
-        <input type='text' className='border-black border' />
-        <p>Add a description to this House.</p>
-        <input type='text' className='border-black border' />
+      <form action='' className='flex flex-col'>
+        <label htmlFor='houseName'>What will be the name of the House?</label>
+        <input
+          id='houseName'
+          type='text'
+          className='border-black border'
+          value={houseName}
+          onChange={handleHouseName}
+        />
+        <label htmlFor='houseSummary'>Add a description to this House.</label>
+        <input
+          id='houseSummary'
+          value={houseSummary}
+          onChange={handleSummary}
+          type='text'
+          className='border-black border'
+        />
       </form>
-      <Button>Create House</Button>
+      <Button onClick={onSubmit}>Create House</Button>
     </div>
   );
 };
