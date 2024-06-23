0a7286ff3ca2e1643e1d75dec445e3fb6d494631
Rcatarata <Rcatarata@gmail.com>
2024-06-22 20:27:14 -0400

    Update page.tsx

--- artemis-wrath/app/dashboard/(routes)/house/page.tsx
+++ artemis-wrath/app/dashboard/(routes)/house/page.tsx
@@ -1,24 +1,35 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSideBar } from '@/hooks/use-sidebar';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import Loader from '../../_components/Loader';
 
 const House = () => {
   const activeSidebar = useSideBar();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     activeSidebar.onChange('guild');
   }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
     <div className='w-screen sm:w-[calc(100vw-96px)] lg:w-[calc(100vw-256px)]'>
       <h1>House</h1>
       <Button>
-        <Link href='/dashboard/house/create'>Create a House</Link>
+        <Link href='/dashboard/house/create' onClick={() => setLoading(true)}>
+          Create a House
+        </Link>
       </Button>
       <Button>
-        <Link href='/dashboard/house/browse'>Browse Houses</Link>
+        <Link href='/dashboard/house/browse' onClick={() => setLoading(true)}>
+          Browse Houses
+        </Link>
       </Button>
     </div>
   );
